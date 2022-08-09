c29d8f191585ddcde48625f6eb5a1531290886e9
Marcela Torro <99346289+Maahtoro@users.noreply.github.com>
2022-08-09 13:43:54 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const servidor = require("./config/servidor")
+
+const app = servidor.app
+
+const porta = servidor.porta
+
+const consign = require('consign')
+consign().include('./routes').into(app)
+
+app.listen(porta)
