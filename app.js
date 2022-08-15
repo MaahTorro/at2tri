635bd3362690e820308b4471af4b63e4a7ceba5c
Marcela Torro <99346289+Maahtoro@users.noreply.github.com>
2022-08-15 12:34:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -10,10 +10,3 @@ require('./routes/desfazer')(app)
 app.listen(porta, () => {
     console.log('http://localhost:' + porta)
 })
-
-// De 4 eh mais gosto#1234 I2SSO31 vazou o penultimo discord dela ai boykkskkkkkkkkkkkkkkkk galaxyysimm
-// epoca q vc FARMAVA webnamorado ha ha ha ha
-// to brincando coe KKKKKK
-//vai comer vai...queria eu comer :(
- // come cmg me leva junto ent
- // levo jaja tou ai dnv nao ironicamente kkkk okok tchau linda qlqr coisa chama no zap ta vai la bj bjbjs
